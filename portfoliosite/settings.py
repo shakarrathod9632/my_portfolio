3ceb2e67c61f77073d610db96569b9a0ecd0059c
shakarrathod9632 <sakkurathod8585@gmail.com>
2025-11-07 16:02:42 +0530

    updated code

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-0gdr@ohbp@bku4f%^*qogncqwzaygo!#2ij5e4zsg^m=5+6w2j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
